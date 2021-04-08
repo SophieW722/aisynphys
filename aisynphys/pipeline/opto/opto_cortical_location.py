aaef54a1b057d58ede960d80f4f5e782f74e7dac
Megan Kratz <meganbkratz@gmail.com>
2021-04-08 13:24:09 -0400

    update cortical location to db19

--- aisynphys/pipeline/opto/opto_cortical_location.py
+++ aisynphys/pipeline/opto/opto_cortical_location.py
@@ -8,11 +8,12 @@ from .opto_experiment import OptoExperimentPipelineModule, load_experiment
 from collections import OrderedDict
 import datetime, os
 import json
+import numpy as np
 from aisynphys import config
-
+from pyqtgraph.debug import Profiler
 
 class OptoCortexLocationPipelineModule(DatabasePipelineModule):
-    """Imports cell morphology data for each experiment
+    """Imports cell and site location data for each experiment
     """
     name = 'opto_cortical_location'
     dependencies = [OptoExperimentPipelineModule]
@@ -24,14 +25,15 @@ class OptoCortexLocationPipelineModule(DatabasePipelineModule):
         job_id = job['job_id'] ## an expt id
         errors = []
 
+        prof = Profiler('opto_cortical_location', disabled=True)
+
         expt = load_experiment(job_id)
+        expt_entry = db.experiment_from_ext_id(expt.ext_id, session=session)
+
+        prof.mark('got experiment')
 
         try:
             # look up slice record in DB
-            # try:
-            #     ts = expt.slice_timestamp
-            # except KeyError:
-            #     ts = 0.0
             ts = expt.info.get('slice_info', {}).get('__timestamp__')
             if ts is None:
                 ts = 0.0
@@ -54,9 +56,12 @@ class OptoCortexLocationPipelineModule(DatabasePipelineModule):
                                     p1, p2 = cortex['piaPos'], cortex['wmPos']
                                     cortex['pia_to_wm_distance'] = ((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)**0.5
                                 cortex['layerBounds_percentDepth'] = item.get('roiState', {}).get('layerBounds_percentDepth')
+                                cortex['sliceAngle'] = item.get('sliceAngle')
                                 break
             if len(cortex) == 0:
                 errors.append('No cortex marker found in connections file or mosaic file.')
+
+            prof.mark('got cortex info')
 
             site_entry = db.CorticalSite(
                     pia_to_wm_distance=cortex.get('pia_to_wm_distance'),
@@ -64,16 +69,23 @@ class OptoCortexLocationPipelineModule(DatabasePipelineModule):
                     wm_position=cortex.get('wmPos'),
                     layer_boundaries=cortex.get('layerBounds_percentDepth'),
                     #brain_region=None,     # The name of the brain region for the site.
+                    meta={'slice_angle':cortex.get('sliceAngle')}
                     )
 
             site_entry.slice = slice_entry
-            site_entry.experiment = db.experiment_from_ext_id(expt.ext_id, session=session)
+            site_entry.experiment = expt_entry
             session.add(site_entry)
 
+            prof.mark('created site entry')
 
-            for cell_id, cell in expt.cells.items():
+            cells = expt.cells.items()
+
+            prof.mark('loaded cells')
+
+            for cell_id, cell in cells:
                 layer_depth = None
                 fractional_layer_depth = None
+                layer_thickness = None
                 if cortex.get('layerBounds_percentDepth') is not None:
                     frac_layer_bounds = cortex['layerBounds_percentDepth'].get('L'+cell.target_layer)
                     if frac_layer_bounds is None:
@@ -82,15 +94,19 @@ class OptoCortexLocationPipelineModule(DatabasePipelineModule):
                     else:
                         layer_depth = (cell.percent_depth-frac_layer_bounds[0])*cortex['pia_to_wm_distance'] 
                         fracional_layer_depth = (cell.percent_depth-frac_layer_bounds[0])/(frac_layer_bounds[1]-frac_layer_bounds[0]) 
+                        layer_thickness = (frac_layer_bounds[1]-frac_layer_bounds[0])/cortex['pia_to_wm_distance']
                 loc_entry = db.CorticalCellLocation(
                     #cell_id=cell.cell_id,
-                    layer=cell.target_layer,
+                    cortical_layer=cell.target_layer,
                     distance_to_pia=cell.distance_to_pia,
                     distance_to_wm=cell.distance_to_wm,
                     fractional_depth=cell.percent_depth,
                     layer_depth=layer_depth,                            # Absolute depth within the layer in m.
+                    layer_thickness=layer_thickness,
                     fractional_layer_depth=fractional_layer_depth,      # Fractional depth within the cells layer.
-                    #position=None,                                      # 2D array, position of cell in slice image coordinates (in m) -- i think this is for having lims images
+                    position=None,                                      # 2D array, position of cell in slice image coordinates (in m) -- i think this is for having lims images?
+                                                                        #      - possibly for us this should just be the x,y position of the cell - but this would be redundant, so 
+                                                                        #        I'll leave it blank for now so it can't accidentally be misused
                 )
 
                 q = session.query(db.Cell)
@@ -105,6 +121,24 @@ class OptoCortexLocationPipelineModule(DatabasePipelineModule):
                     raise Exception("Found %i cell entries for experiment %s, cell %s" %(len(cell_entry),job_id, cell_id))
                 loc_entry.cortical_site = site_entry
                 session.add(loc_entry)
+
+            prof.mark('created cell_location entries')
+
+            ## fill in lateral_distance and vertical_distance for each pair
+            if cortex.get('sliceAngle') is not None:
+                angle = cortex.get('sliceAngle') * np.pi/180.
+                for pair_entry in expt_entry.pair_list:
+                    pre_cell_pos = pair_entry.pre_cell.position
+                    post_cell_pos = pair_entry.post_cell.position
+                    if pre_cell_pos is None or post_cell_pos is None:
+                        continue
+                    x1, y1 = post_cell_pos[0]-pre_cell_pos[0], post_cell_pos[1] - pre_cell_pos[1]
+                    x2 = abs(np.cos(angle)*x1 - np.sin(angle)*y1)
+                    y2 = abs(np.sin(angle)*x1 + np.cos(angle)*y1)
+                    pair_entry.lateral_distance = x2
+                    pair_entry.vertical_distance = y2
+
+            prof.mark('updted pair entries')
 
             return errors
 
