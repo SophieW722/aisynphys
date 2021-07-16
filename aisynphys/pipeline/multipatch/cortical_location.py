aed5f311cb6e0bed5e39464c256c00a0b99706c9
Tom Chartrand <tom.chartrand@alleninstitute.org>
2021-07-16 15:22:49 -0700

    cortical_location lims check in ready_jobs

--- aisynphys/pipeline/multipatch/cortical_location.py
+++ aisynphys/pipeline/multipatch/cortical_location.py
@@ -2,6 +2,7 @@
 For generating DB table describing a cells location within cortex, 
 and adding layer-aligned distance info to the Pair table
 """
+from collections import OrderedDict
 from ..pipeline_module import DatabasePipelineModule
 from .experiment import ExperimentPipelineModule
 from aisynphys import lims
@@ -27,31 +28,10 @@ class CortexLocationPipelineModule(DatabasePipelineModule):
         errors = []
 
         expt = db.experiment_from_ext_id(expt_id, session=session)
-        slice_entry = expt.slice
-
-        try:
-            images = lims.specimen_images(slice_entry.lims_specimen_name)
-            images = [image for image in images if image.get('treatment')=='DAPI']
-            assert len(images) > 0
-            image_series_id = images[0].get('image_series')
-            image_series_resolution = images[0].get('resolution')
-        except (AssertionError, ValueError) as exc:
-            raise ValueError("No LIMS image series found for slice.") from exc
-
-        try:
-            lims_cell_cluster_id = expt.meta.get('lims_cell_cluster_id')
-            lims_cell_info = lims.cluster_cells(lims_cell_cluster_id)
-            lims_ids = [cell.meta.get('lims_specimen_id') for cell in expt.cell_list]
-            soma_centers = {cell['id']: (cell['x_coord'], cell['y_coord']) 
-                            for cell in lims_cell_info}
-            soma_centers = {cell: coords for cell, coords in soma_centers.items() 
-                            if all(coords) and cell in lims_ids}
-            assert len(soma_centers) > 0
-        except (AssertionError, ValueError) as exc:
-            raise ValueError("No cell coordinates found for cell cluster.") from exc
+        image_series_id, soma_centers, image_series_resolution = get_lims_info(expt)
         
         results, cell_errors = get_depths_slice(image_series_id, soma_centers,
-                                                species=slice_entry.species,
+                                                species=expt.slice.species,
                                                 resolution=image_series_resolution)
         if len(results)==0:
             errors.append("No cells passed depth calculation.")
@@ -116,10 +96,39 @@ class CortexLocationPipelineModule(DatabasePipelineModule):
         """
         db = self.database
         return (session.query(db.CorticalCellLocation)
-            .join(db.Cell)
-            .join(db.Experiment)
-            .filter(db.Experiment.ext_id.in_(job_ids))
-            .all())
+                .filter(db.CorticalCellLocation.cell_id==db.Cell.id)
+                .filter(db.Cell.experiment_id==db.Experiment.id)
+                .filter(db.Experiment.ext_id.in_(job_ids)).all())
+
+    def ready_jobs(self):
+        """Return an ordered dict of all jobs that are ready to be processed (all dependencies are present)
+        and the dates that dependencies were created.
+        """
+        db = self.database
+        # All experiments and their creation times in the DB
+        expts = self.pipeline.get_module('experiment').finished_jobs()
+
+        session = db.session()
+        ready = OrderedDict()
+            
+        for expt_id, (expt_mtime, success) in expts.items():
+            if success is not True:
+                continue
+
+            q = session.query(db.Experiment)
+            q = q.filter(db.Experiment.ext_id==expt_id)
+            expt = q.all()[0]
+
+            try:
+                # just check for complete lims info, no need to compare values
+                image_series_id, _, _ = get_lims_info(expt)
+                polys = lims.query_for_layer_polygons(image_series_id)
+                assert len(polys) > 0
+            except (AssertionError, ValueError):
+                continue
+            ready[expt_id] = {'dep_time': expt_mtime}
+        
+        return ready
 
 def get_pair_distances(pair, pia_direction):
     l1 = np.array(pair.pre_cell.cortical_location.position)
@@ -137,3 +146,21 @@ def get_lims_layers():
         lims_cache = {spec_id:layer.lstrip('Layer') for layer, spec_id in lims_q if layer is not None}
 
     return lims_cache
+
+def get_lims_info(expt):
+    images = lims.specimen_images(expt.slice.lims_specimen_name)
+    images = [image for image in images if image.get('treatment')=='DAPI']
+    assert len(images) > 0
+    image_series_id = images[0].get('image_series')
+    image_series_resolution = images[0].get('resolution')
+    
+    lims_cell_cluster_id = expt.meta.get('lims_cell_cluster_id')
+    lims_cell_info = lims.cluster_cells(lims_cell_cluster_id)
+    lims_ids = [cell.meta.get('lims_specimen_id') for cell in expt.cell_list]
+    soma_centers = {cell['id']: (cell['x_coord'], cell['y_coord']) 
+                    for cell in lims_cell_info}
+    soma_centers = {cell: coords for cell, coords in soma_centers.items() 
+                    if all(coords) and cell in lims_ids}
+    assert len(soma_centers) > 0
+        
+    return image_series_id, soma_centers, image_series_resolution
