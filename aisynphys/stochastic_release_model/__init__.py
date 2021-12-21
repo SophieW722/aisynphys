d54de611e7b4fb2f56ecb4da3259e5440e2c696a
Luke Campagnola <luke.campagnola@gmail.com>
2021-12-21 15:57:59 -0800

    convert stochastic model into subpackage

---
+++ aisynphys/stochastic_release_model/__init__.py
@@ -0,0 +1,3 @@
+from .model import StochasticReleaseModel, StochasticReleaseModelResult
+from .model_runner import StochasticModelRunner, CombinedModelRunner, ParameterSpace
+from .file_management import model_result_cache_path, load_cache_file, load_cached_model_results, list_cached_results, load_spca_results
