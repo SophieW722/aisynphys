105f847e6c44bb30b96a20372554ce8fbaa9c798
Luke Campagnola <luke.campagnola@gmail.com>
2021-12-22 13:49:40 -0800

    minor fix

--- aisynphys/stochastic_release_model/__init__.py
+++ aisynphys/stochastic_release_model/__init__.py
@@ -1,3 +1,4 @@
 from .model import StochasticReleaseModel, StochasticReleaseModelResult
 from .model_runner import StochasticModelRunner, CombinedModelRunner, ParameterSpace
-from .file_management import model_result_cache_path, load_cache_file, load_cached_model_results, list_cached_results, load_spca_results
+from .file_management import model_result_cache_path, load_cache_file, load_cached_model_results, list_cached_results
+from .reduction import load_spca_results
