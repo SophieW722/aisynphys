472ff1a5b12b28ca397981a0258a8ec9f36e35f5
Luke Campagnola <lukec@alleninstitute.org>
2021-12-22 16:31:55 -0800

    minor fixes, spca reduction script working

--- aisynphys/stochastic_release_model/reduction.py
+++ aisynphys/stochastic_release_model/reduction.py
@@ -32,7 +32,8 @@ def reduce_model_results(output_file=None, likelihood_only=False, cache_path=Non
     """
     if output_file is None:
         output_file = default_spca_file(likelihood_only)
-    cache_files = [result[1] for result in list_cached_results(cache_path)]
+    cache_files = [result[1] for result in list_cached_results(cache_path)][:10]
+    print(f"Generating SPCA reduction from {len(cache_files)} cached model files; writing to {output_file}")
 
     ## Load all model outputs into a single array
     agg_result, cache_files, param_space = load_cached_model_results(cache_files, db=db)
--- tools/stochastic_model_reduction.py
+++ tools/stochastic_model_reduction.py
@@ -5,11 +5,18 @@ Depending on the number of synapses to process and the size of the model paramet
 may consume a large amount of memory (~1TB for original coarse matrix data) and CPU time.
 
 """
-
+import argparse
+from aisynphys import config
 from aisynphys.stochastic_release_model.reduction import reduce_model_results
+
+
+parser = argparse.ArgumentParser(parents=[config.parser])
+parser.add_argument('--output-file', type=str, default=None, dest='output_file', help="Optional file name to write")
+parser.add_argument('--cache-path', type=str, default=None, dest='cache_path', help="Optional path to model cache files")
+args = parser.parse_args()
 
 
 for likelihood_only in (False, True):
     # use default cache path and output file
-    reduce_model_results(likelihood_only=likelihood_only)
+    reduce_model_results(likelihood_only=likelihood_only, cache_path=args.cache_path, output_file=args.output_file)
 
