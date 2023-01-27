66d147da3bb4973b135ea33a479abe716d0e322b
Luke Campagnola <lukec@alleninstitute.org>
2023-01-26 16:30:56 -0800

    make edit a little easier

--- edit.py
+++ edit.py
@@ -2,11 +2,28 @@ import os, sys, base64, pickle, json
 
 url_file = 'download_urls'
 tmp_file = url_file + '.tmp'
+
+
+def check_json(filename):
+    try:
+        urls = json.load(open(tmp_file, 'r'))
+    except Exception as exc:
+        sys.excepthook(*sys.exc_info())
+        return False
+    return True
+
+
+
 urls = pickle.loads(base64.b64decode(open(url_file, 'r').read()))
 js = json.dumps(urls, indent='    ')
 open(tmp_file, 'w').write(js)
 
-os.system('nano ' + tmp_file)
+while True:
+    os.system('nano ' + tmp_file)
+    if check_json(tmp_file):
+        break
+    input("Press enter to edit again..")
+    
 
 urls = json.load(open(tmp_file, 'r'))
 b64p = base64.b64encode(pickle.dumps(urls))
